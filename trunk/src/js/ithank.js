90e35dccf37682a43e45f8fd7514abac3081c94b
livibetter <livibetter@dfdbd1e4-0175-11de-b556-9305e745af9e>
2009-03-03 06:20:08 +0000

    Added Disqus integration

--- trunk/src/js/ithank.js
+++ trunk/src/js/ithank.js
@@ -75,3 +75,18 @@ function init_header() {
 
 google.setOnLoadCallback(init_header);
 
+
+// Initialize Disqus
+// Based on http://disqus.com/integrate/ithank/generic/
+google.setOnLoadCallback(function() {
+  var links = document.getElementsByTagName('a');
+  var query = '?';
+  for(var i = 0; i < links.length; i++) {
+    if(links[i].href.indexOf('#disqus_thread') >= 0) {
+      query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+      }
+    }
+  // document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/ithank/get_num_replies.js' + query + '"></' + 'script>');
+  $.getScript("http://disqus.com/forums/ithank/get_num_replies.js" + query);
+  });
+
